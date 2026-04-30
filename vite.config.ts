1c8949d344141eb1b9d8bdb9d4baa2bf19aece75
Shri K <shrilaks75@gmail.com>
2026-04-30 07:16:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()]
-})import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()]
 })
